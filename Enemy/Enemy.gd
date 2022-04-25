dec64969cfa44138dda7e6c4d1cb2cc1ab44c3a4
Excailibur <72610128+Excailibur@users.noreply.github.com>
2022-04-25 16:21:06 -0400

    complete

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -11,5 +11,5 @@ func _physics_process(_delta):
 func die():
 	queue_free()
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(_anim_name):
 	$AnimationPlayer.play("Idle")
